daa562368fe600a7b89812f978829697ae904bf6
sayed94h <skazimi951@yahoo.com>
2021-07-09 10:14:39 +0200

    added new feature so that user can search for a specific https status code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# HTTP STATUS CODES IN QUIZ GAME
+## HTTP STATUS CODES IN QUIZ GAME
 
-# HTTP RESPONSE CODES IN QUIZ GAME
+## HTTP RESPONSE CODES IN QUIZ GAME
 
 I created this small QUIZ on HTTP Status or response codes to help you learn HTTP Status Codes fast and easy.
 
@@ -8,3 +8,12 @@ If you would like to see the logics click [here](https://github.com/Sayed94h/htt
 If you would like to see the Live Demo click [here](https://sayed94h.github.io/http_status_codes_quiz/)
 
 Play around and Have Fun!
+
+> ### About me
+
+I'm _Sayed Khalil Kazimi_ a Full Stack Developer. If you would like to know more about me, please visit my [portfolio website](https://everydaysmarter.herokuapp.com/).
+
+You can also find me on [LinkedIn](https://www.linkedin.com/in/sayed-kazimi-0507/).
+
+# Have Beautiful Times
+
--- index.js
+++ index.js
@@ -6,41 +6,81 @@ const game_Question = document.getElementById("game_question");
 const game_question_section = document.querySelector(".game_questions");
 const game_answers = document.getElementById("game_answers");
 const game_score = document.getElementById("game_score");
+const currentYear = document.getElementById("current_year");
+
 let real_time_score = 0;
 // create questions and answer options dynamically
 async function question_answers() {
-	game_answers.innerHTML = "";
-	const question = await status_code_game.generateQuestion();
-	const answers = await status_code_game.generateAnswer();
-	game_Question.textContent = `${question}`;
-	for (let i = 0; i < answers.length; i++) {
-		game_answers.appendChild(answers[i]);
-	}
+    game_answers.innerHTML = "";
+    const question = await status_code_game.generateQuestion();
+    const answers = await status_code_game.generateAnswer();
+    game_Question.textContent = `${question}`;
+    for (let i = 0; i < answers.length; i++) {
+        game_answers.appendChild(answers[i]);
+    }
 }
 next.onclick = question_answers;
+
 function onStart_onLoad() {
-	next.style.display = "initial";
-	game_question_section.style.display = "initial";
-	question_answers();
-	start_game.style.display = "none";
+    next.style.display = "initial";
+    game_question_section.style.display = "initial";
+    question_answers();
+    start_game.style.display = "none";
 }
 start_game.onclick = onStart_onLoad;
-// window.onload = onStart_onLoad;
 // check the user answer
-game_answers.onclick = function (event) {
-	const targetEl = event.target;
-	const targetValue = event.target.textContent;
-	const checkAnswer = status_code_game.check_user_answer(targetValue);
-	if (checkAnswer === 1) {
-		targetEl.style =
-			"background-color: green; color:white; border: 1px green solid; outline: none;";
-		real_time_score += checkAnswer;
-		game_score.textContent = `${real_time_score}`;
-	} else {
-		targetEl.style =
-			"background-color: red; color:white; border: 1px red solid; outline: none;";
-		const correctAnswerEl = document.getElementById("child_div");
-		correctAnswerEl.style =
-			"background-color: green; color:white; border: 1px green solid; outline: none;";
-	}
+game_answers.onclick = function(event) {
+    const targetEl = event.target;
+    const targetValue = event.target.textContent;
+    const checkAnswer = status_code_game.check_user_answer(targetValue);
+    if (checkAnswer === 1) {
+        targetEl.style =
+            "background-color: green; color:white; border: 1px green solid; outline: none;";
+        real_time_score += checkAnswer;
+        game_score.textContent = `${real_time_score}`;
+    } else {
+        targetEl.style =
+            "background-color: red; color:white; border: 1px red solid; outline: none;";
+        const correctAnswerEl = document.getElementById("child_div");
+        correctAnswerEl.style =
+            "background-color: green; color:white; border: 1px green solid; outline: none;";
+    }
 };
+// search status code
+const searchInput = document.getElementById("search_status_code");
+const searchResult = document.getElementById("search_result");
+
+searchInput.onkeyup = function() {
+        debugger
+        const userInput = searchInput.value
+        const onlyChar = userInput.replaceAll(/\s/g, '')
+        let result = ""
+        if (onlyChar.length > 2) {
+            if (!onlyChar.includes("x")) {
+                const toNum = Number(onlyChar)
+                result = status_code_game.searchStatusCode(toNum)
+                if (result === "undefined") {
+                    searchResult.innerText = "Sorry, could not find explanation for the status/response code " + onlyChar
+                    return
+                }
+                searchResult.innerText = result
+
+            } else {
+                result = status_code_game.searchStatusCode(onlyChar)
+                if (result === "undefined") {
+                    searchResult.innerText = "Sorry, could not find explanation for the status/response code " + onlyChar
+                    return
+                }
+                searchResult.innerText = result
+            }
+        }
+    }
+    // set the current year in the footer
+
+function cY() {
+    const cDate = new Date()
+    const onlyYear = cDate.getFullYear()
+    currentYear.innerText = onlyYear
+}
+
+cY()
